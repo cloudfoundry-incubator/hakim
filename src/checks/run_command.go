b662706bf569112a1123cc79b84ab8883c905813
Micah Young <myoung@pivotal.io>
2016-02-01 17:00:32 -0500

    Fix runCommand execution
    Don't use tempfile script - run the command directly
    Refactor runCommand
    
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- src/checks/run_command.go
+++ src/checks/run_command.go
@@ -2,49 +2,18 @@ package checks
 
 import (
 	"bytes"
-	"io/ioutil"
-	"os"
 	"os/exec"
 )
 
 func runCommand(cmd string) (string, string, error) {
-	file, err := ioutil.TempFile(os.TempDir(), "hakim")
-	if err != nil {
-		return "", "", err
-	}
-	defer os.Remove(file.Name())
+	execCmd := exec.Command("powershell", "-noprofile", "-noninteractive", "-command", cmd)
 
-	_, err = file.Write([]byte(cmd))
-	if err != nil {
-		return "", "", err
-	}
+	var stdout bytes.Buffer
+	execCmd.Stdout = &stdout
 
-	err = file.Close()
-	if err != nil {
-		return "", "", err
-	}
+	var stderr bytes.Buffer
+	execCmd.Stderr = &stderr
 
-	err = os.Rename(file.Name(), file.Name()+".ps1")
-	if err != nil {
-		return "", "", err
-	}
-
-	execCmd := exec.Command("powershell", "-noprofile", "-noninteractive", file.Name()+".ps1")
-
-	execCmd.Stdin = bytes.NewBufferString(cmd)
-
-	stdout := bytes.NewBufferString("")
-	execCmd.Stdout = stdout
-
-	stderr := bytes.NewBufferString("")
-	execCmd.Stderr = stderr
-
-	err = execCmd.Run()
-
-	stdoutStr := string(stdout.Bytes())
-	stderrStr := string(stderr.Bytes())
-
-	execCmd.Wait()
-
-	return stdoutStr, stderrStr, err
+	err := execCmd.Run()
+	return stdout.String(), stderr.String(), err
 }
