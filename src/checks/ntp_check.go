6d9a9dd0c35c14b44ef1191536f5639a0d10db94
Matthew Horan <mhoran@pivotal.io>
2016-02-05 12:00:44 -0500

    Only outbound firewall rules need to exist for NTP
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- src/checks/ntp_check.go
+++ src/checks/ntp_check.go
@@ -15,7 +15,7 @@ There was an error detecting ntp synchronization on your machine.
 An accurate system clock is essential for internal Cloud Foundry metric reports.
 
 Please configure your NTP settings, if not already done.
-We recommend that your firewall have inbound and outbound rules set for UDP on port 123.
+We recommend that your firewall have outbound rules set for UDP on port 123.
 In addition, ensure that your 'DnsCache' service is running.  ` + "Error: \n\n" + stdout)
 	}
 }
